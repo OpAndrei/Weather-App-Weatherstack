e7933726faa8f1a83303b76c8fc0039b1d66d434
OpAndrei <andrei.oprean13@yahoo.com>
2022-02-08 13:22:24 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React from 'react';
+import Axios from 'axios';
 import './App.css';
+import DisplayWeather from './components/DisplayWeather';
+import Navbar from './components/Navbar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //State
+  state = {
+    //Variables
+    coords:{
+      latitude: 44,
+      longitude: 24
+    },
+    data:{},
+    inputData:''
+  }
+
+  componentDidMount(){
+    //get device location
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) =>{
+        let newCoords = {
+          latitude:position.coords.latitude,
+          longitude:position.coords.longitude
+        }
+        this.setState({coords:newCoords});
+
+        //Api call
+        Axios.get(`http://api.weatherstack.com/current?access_key=437a67c043edc32af1a7ad22e5da760c&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {let weatherData= {
+              location:res.data.location.name,
+              temperature:res.data.current.temperature,
+              description:res.data.current.weather_descriptions[0],
+              region:res.data.location.region,
+              country:res.data.location.country,
+              wind_speed:res.data.current.wind_speed,
+              pressure:res.data.current.pressure,
+              precip:res.data.current.precip,
+              humidity:res.data.current.humidity,
+              img:res.data.current.weather_icons
+        }
+
+        this.setState({data:weatherData});
+
+      })
+      })
+    }else{
+      console.log('Not supported')
+    }
+  }
+
+ //Track the input field
+      change = (value) => {
+       this.setState({inputData:value})
+      }
+
+      changeWeather = (event) =>{
+        event.preventDefault();
+
+        //Api call
+        Axios.get(`http://api.weatherstack.com/current?access_key=437a67c043edc32af1a7ad22e5da760c&query=${this.state.inputData}`).then(res => {
+          let weatherData= {
+            location:res.data.location.name,
+            temperature:res.data.current.temperature,
+            description:res.data.current.weather_descriptions[0],
+            region:res.data.location.region,
+            country:res.data.location.country,
+            wind_speed:res.data.current.wind_speed,
+            pressure:res.data.current.pressure,
+            precip:res.data.current.precip,
+            humidity:res.data.current.humidity,
+            img:res.data.current.weather_icons
+      }
+
+      this.setState({data:weatherData});
+        })
+      }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className = 'container'>
+        <Navbar changeWeather = {this.changeWeather} changeRegion = {this.change}/>
+        <DisplayWeather weatherData = {this.state.data} />
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
